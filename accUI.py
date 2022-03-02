ea6846573622f615f331e3572a961242f6ba6c54
Rachel Ang <ang,minghui.rachel@gmail.com>
2022-03-02 08:24:22 +0800

    made corrections for option c

--- accUI.py
+++ accUI.py
@@ -1,54 +1,89 @@
-import SavingsAccClass
-import CurrentAccClass
-import schedule
+from FixedDepAccClass import FixedDepAcc
+from SavingsAccClass import SavingsAcc
+from CurrentAccClass import CurrentAcc
+from schedule import every, run_pending
 import time
 
 print('Welcome to deposit account management system')
 print('Please select a letter to check the type of account to check on:')
 print("========== (a). Savings Account ============")
+print("========== (b). Current Account ============")
+print("========== (c). Fixed Deposit Account ============")
 
 accType = input('Account to check: ')
 
-savingsAccInstance = SavingsAccClass.SavingsAcc()
-currentAccInstance = CurrentAccClass.CurrentAcc()
+savingsAccInstance = SavingsAcc()
+currentAccInstance = CurrentAcc()
+fixedDepAccInstance = FixedDepAcc()
 
 def job():
   print("Interests accrued!")
 
 def currentBankPost():
-    currentAccInstance.bankPostList.append("Current balance after interest accrued: " + str(currentAccInstance.accBalance))
+  currentAccInstance.bankPostList.append("Current balance after interest accrued: " + str(currentAccInstance.accBalance))
 
 def savingsBankPost():
-    savingsAccInstance.bankPostList.append("Current balance after interest accrued: " + str(savingsAccInstance.accBalance))
+  savingsAccInstance.bankPostList.append("Current balance after interest accrued: " + str(savingsAccInstance.accBalance))
+
+def fixedDepBankPost():
+  fixedDepAccInstance.bankPostList.append("Current balance after interest accrued: " + str(fixedDepAccInstance.accBalance))
 
 def currentAccrueInt():
-    currentAccInstance.accBalance = currentAccInstance.accBalance * (1 + currentAccInstance.intRate)
+  currentAccInstance.accBalance = currentAccInstance.accBalance * (1 + currentAccInstance.intRate)
 
 def savingsAccrueInt():
-    savingsAccInstance.accBalance = savingsAccInstance.accBalance * (1 + savingsAccInstance.intRate)
+  savingsAccInstance.accBalance = savingsAccInstance.accBalance * (1 + savingsAccInstance.intRate)
+
+def fixedDepAccrueInt():
+  fixedDepAccInstance.accBalance = fixedDepAccInstance.accBalance * (1 + fixedDepAccInstance.intRate)
+
+def chfToSgdConverter():
+  if fixedDepAccInstance.curr.value > 1:
+    fixedDepAccInstance.accBalance /= fixedDepAccInstance.curr.value
+  elif fixedDepAccInstance.curr.value < 1:
+    fixedDepAccInstance.accBalance *= fixedDepAccInstance.curr.value
+    print(fixedDepAccInstance.accBalance)
 
 if accType == 'a':
   print(savingsAccInstance.accNum)
 
-  schedule.every(savingsAccInstance.intFreq.value).seconds.do(job)
-  schedule.every(savingsAccInstance.intFreq.value).seconds.do(savingsAccrueInt)
-  schedule.every(savingsAccInstance.intFreq.value + 1).seconds.do(savingsBankPost)
+  every(savingsAccInstance.intFreq.value).seconds.do(job)
+  every(savingsAccInstance.intFreq.value).seconds.do(savingsAccrueInt)
+  every(savingsAccInstance.intFreq.value + 1).seconds.do(savingsBankPost)
   t_end = time.time() + 30
 
   while time.time() < t_end:
-    schedule.run_pending()
+    run_pending()
 
   print(savingsAccInstance.bankPostList)
 
 elif accType == 'b':
   print(currentAccInstance.accNum)
 
-  schedule.every(currentAccInstance.intFreq.value).seconds.do(job)
-  schedule.every(currentAccInstance.intFreq.value).seconds.do(currentAccrueInt)
-  schedule.every(currentAccInstance.intFreq.value + 1).seconds.do(currentBankPost)
+  every(currentAccInstance.intFreq.value).seconds.do(job)
+  every(currentAccInstance.intFreq.value).seconds.do(currentAccrueInt)
+  every(currentAccInstance.intFreq.value + 1).seconds.do(currentBankPost)
   t_end = time.time() + 30
 
   while time.time() < t_end:
-    schedule.run_pending()
+    run_pending()
 
   print(currentAccInstance.bankPostList)
+
+elif accType == 'c':
+  print(fixedDepAccInstance.accNum)
+  print("Would you like to view amount in sgd?")
+  convertAns = input('Y/N: ')
+
+  if convertAns == 'Y':
+    chfToSgdConverter()
+
+  every(fixedDepAccInstance.intFreq.value).seconds.do(job)
+  every(fixedDepAccInstance.intFreq.value).seconds.do(fixedDepAccrueInt)
+  every(fixedDepAccInstance.intFreq.value + 1).seconds.do(fixedDepBankPost)
+  t_end = time.time() + 30
+
+  while time.time() < t_end:
+    run_pending()
+
+  print(fixedDepAccInstance.bankPostList)
