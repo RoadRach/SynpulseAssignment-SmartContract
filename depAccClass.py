2c2f7ace096bedcccad2c63c93d5b2752e899842
Rachel Ang <ang,minghui.rachel@gmail.com>
2022-03-02 08:28:58 +0800

    housekeeping

--- depAccClass.py
+++ depAccClass.py
@@ -12,7 +12,3 @@ class DepAcc():
   intFreq = intFreqEnum.intFreqEnum.NA
   accBalance = 50000.00
   bankPostList = []
-
-  # def accrueInt():
-  #   DepAcc.accBalance *= (1 + DepAcc.intRate)
-  #   DepAcc.bankPostList.append('Interest of '+ DepAcc.intRate+'had been added. Current account balance is '+ DepAcc.accBalance)
