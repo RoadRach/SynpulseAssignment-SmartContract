5f53a6ce889852f6f587836a3a6bf7787738e79d
Rachel Ang <ang,minghui.rachel@gmail.com>
2022-03-03 13:44:17 +0800

    housekeeping for accUI.py

--- accUI.py
+++ accUI.py
@@ -12,46 +12,19 @@ print("========== (c). Fixed Deposit Account ============")
 
 accType = input('Account to check: ')
 
-savingsAccInstance = SavingsAcc()
-currentAccInstance = CurrentAcc()
-fixedDepAccInstance = FixedDepAcc()
-
-def job():
-  print("Interests accrued!")
-
-def currentBankPost():
-  currentAccInstance.bankPostList.append("Current balance after interest accrued: " + str("{:.2f}".format(currentAccInstance.accBalance)))
-
-def savingsBankPost():
-  savingsAccInstance.bankPostList.append("Current balance after interest accrued: " + str("{:.2f}".format(savingsAccInstance.accBalance)))
-
-def fixedDepBankPost():
-  fixedDepAccInstance.bankPostList.append("Current balance after interest accrued: " + str("{:.2f}".format(fixedDepAccInstance.accBalance)))
-
-def currentAccrueInt():
-  currentAccInstance.accBalance = currentAccInstance.accBalance * (1 + currentAccInstance.intRate)
-
-def savingsAccrueInt():
-  savingsAccInstance.accBalance = savingsAccInstance.accBalance * (1 + savingsAccInstance.intRate)
-
-def fixedDepAccrueInt():
-  fixedDepAccInstance.accBalance = fixedDepAccInstance.accBalance * (1 + fixedDepAccInstance.intRate)
-
-def chfToSgdConverter():
-  if fixedDepAccInstance.curr.value > 1:
-    fixedDepAccInstance.accBalance /= fixedDepAccInstance.curr.value
-    print(fixedDepAccInstance.accBalance)
-  elif fixedDepAccInstance.curr.value < 1:
-    fixedDepAccInstance.accBalance *= fixedDepAccInstance.curr.value
-    print(fixedDepAccInstance.accBalance)
-
 if accType == 'a':
+  savingsAccInstance = SavingsAcc()
   print(savingsAccInstance.accNum)
 
-  every(savingsAccInstance.intFreq.value).seconds.do(job)
+  def savingsBankPost():
+    savingsAccInstance.bankPostList.append("Current balance after interest accrued: " + str("{:.2f}".format(savingsAccInstance.accBalance)))
+
+  def savingsAccrueInt():
+    savingsAccInstance.accBalance = savingsAccInstance.accBalance * (1 + savingsAccInstance.intRate)
+
   every(savingsAccInstance.intFreq.value).seconds.do(savingsAccrueInt)
   every(savingsAccInstance.intFreq.value + 1).seconds.do(savingsBankPost)
-  t_end = time.time() + 60
+  t_end = time.time() + 20
 
   while time.time() < t_end:
     run_pending()
@@ -59,12 +32,18 @@ if accType == 'a':
   print(savingsAccInstance.bankPostList)
 
 elif accType == 'b':
+  currentAccInstance = CurrentAcc()
   print(currentAccInstance.accNum)
 
-  every(currentAccInstance.intFreq.value).seconds.do(job)
+  def currentBankPost():
+    currentAccInstance.bankPostList.append("Current balance after interest accrued: " + str("{:.2f}".format(currentAccInstance.accBalance)))
+
+  def currentAccrueInt():
+    currentAccInstance.accBalance = currentAccInstance.accBalance * (1 + currentAccInstance.intRate)
+
   every(currentAccInstance.intFreq.value).seconds.do(currentAccrueInt)
   every(currentAccInstance.intFreq.value + 1).seconds.do(currentBankPost)
-  t_end = time.time() + 30
+  t_end = time.time() + 20
 
   while time.time() < t_end:
     run_pending()
@@ -72,17 +51,31 @@ elif accType == 'b':
   print(currentAccInstance.bankPostList)
 
 elif accType == 'c':
+  fixedDepAccInstance = FixedDepAcc()
   print(fixedDepAccInstance.accNum)
   print("Would you like to view amount in sgd?")
   convertAns = input('Y/N: ')
 
+  def fixedDepBankPost():
+    fixedDepAccInstance.bankPostList.append("Current balance after interest accrued: " + str("{:.2f}".format(fixedDepAccInstance.accBalance)))
+
+  def fixedDepAccrueInt():
+    fixedDepAccInstance.accBalance = fixedDepAccInstance.accBalance * (1 + fixedDepAccInstance.intRate)
+
+  def chfToSgdConverter():
+    if fixedDepAccInstance.curr.value > 1:
+      fixedDepAccInstance.accBalance /= fixedDepAccInstance.curr.value
+      print(fixedDepAccInstance.accBalance)
+    elif fixedDepAccInstance.curr.value < 1:
+      fixedDepAccInstance.accBalance *= fixedDepAccInstance.curr.value
+      print(fixedDepAccInstance.accBalance)
+
   if convertAns == 'Y':
     chfToSgdConverter()
 
-  every(fixedDepAccInstance.intFreq.value).seconds.do(job)
   every(fixedDepAccInstance.intFreq.value).seconds.do(fixedDepAccrueInt)
   every(fixedDepAccInstance.intFreq.value + 1).seconds.do(fixedDepBankPost)
-  t_end = time.time() + 30
+  t_end = time.time() + 20
 
   while time.time() < t_end:
     run_pending()
